869d54de224adb22f04d1eb7496c418f142f0857
Yuma Chikazawa <chikazawa.yuma.u7@s.mail.nagoya-u.ac.jp>
2023-05-01 19:28:01 +0900

    gatsby-1つめの記事

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/header.js";
+import Footer from "../components/footer.js";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Header />
+      <h1>About Pageだよ</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default AboutPage;
